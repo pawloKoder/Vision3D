3d3a9d99037f287dc422f4395bc9fe07a587a377
Paweł Wawruch <pawlo@aleg.pl>
2014-03-24 14:31:43 +0100

    Fix rendering trajectories.

--- src/renderers/trajectoryrenderer.cpp
+++ src/renderers/trajectoryrenderer.cpp
@@ -31,14 +31,14 @@ void TrajectoryRenderer::paintImpl()
 	glColor3f(color->getValue().redF(), color->getValue().greenF(),
 		color->getValue().blueF());
 
-	glBegin(GL_LINES);
+	glBegin(GL_LINE_STRIP);
 	QVector3D prev;
 	foreach(auto current, data.vertices)
 	{
 		if((prev - current).length() > level->getValue())
 		{
 			glEnd();
-			glBegin(GL_LINES);
+			glBegin(GL_LINE_STRIP);
 		}
 		glVertex3f(current.x(), current.y(), current.z());
 		prev = current;
